4d309309655b9f9e293195914e70395475049d18
keremalp72 <55767177+keremalp72@users.noreply.github.com>
2024-11-08 02:58:56 +0300

    Add files via upload

--- pricing.js
+++ pricing.js
@@ -2,16 +2,18 @@ const priceChange = document.querySelector("#priceChange");
 
 priceChange.addEventListener("input", change);
 
-const price = document.querySelectorAll(".price");
+const prices = document.querySelectorAll(".price");
 
 function change() {
+  
   if (priceChange.value == 1) {
-    for (const prices of price) {
-      prices.innerText = (parseFloat(prices.innerText) * 10 + 0.09).toFixed(2);
+
+    for (const price of prices) {
+      price.innerHTML = `<span class="dollarsign">$</span> <span class="miktar">${(parseFloat(price.querySelector(".miktar").innerText) * 10 + 0.09).toFixed(2)}</span>`;
     }
   } else if (priceChange.value == 2) {
-    for (const prices of price) {
-      prices.innerText = (parseFloat(prices.innerText) / 10 - 0.01).toFixed(2);
+    for (const price of prices) {
+      price.innerHTML = `<span class="dollarsign">$</span> <span class="miktar">${(parseFloat(price.querySelector(".miktar").innerText) / 10 - 0.01).toFixed(2)}</span>`;
     }
   }
 }
