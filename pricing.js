a444f415f10b28af6aad555bc457f5e994387362
keremalp72 <55767177+keremalp72@users.noreply.github.com>
2024-11-07 20:19:59 +0300

    Add files via upload

---
+++ pricing.js
@@ -0,0 +1,18 @@
+const priceChange = document.querySelector("#priceChange");
+
+priceChange.addEventListener("input", change);
+
+const price = document.querySelectorAll(".price");
+
+function change() {
+  if (priceChange.value == 1) {
+    priceChange.style.backgroundColor = "red";
+    for (const prices of price) {
+      prices.innerText = (parseFloat(prices.innerText) * 10 + 0.09).toFixed(2);
+    }
+  } else if (priceChange.value == 2) {
+    for (const prices of price) {
+      prices.innerText = (parseFloat(prices.innerText) / 10 - 0.01).toFixed(2);
+    }
+  }
+}
